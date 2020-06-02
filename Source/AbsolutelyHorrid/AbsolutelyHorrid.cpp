2d58b55352802df7dc80c139d7e13ed055e4ff8f
Iunave <Quadrivia@protonmail.com>
2020-06-02 14:46:26 +0200

    Changed engine to 4.24.3

--- Source/AbsolutelyHorrid/AbsolutelyHorrid.cpp
+++ Source/AbsolutelyHorrid/AbsolutelyHorrid.cpp
@@ -3,8 +3,4 @@
 #include "AbsolutelyHorrid.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AbsolutelyHorrid, "AbsolutelyHorrid" )
-
-DEFINE_LOG_CATEGORY(HorridLog)
-
-
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AbsolutelyHorrid, "AbsolutelyHorrid" );
